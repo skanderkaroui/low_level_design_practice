ac2900d30219730c2d2a92ca3ef36c5bca939cbd
skanderkaroui <skander.karoui@gmail.com>
2024-11-12 18:03:02 +0100

    encapsulation test

---
+++ basic_poo_principles.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.__acount_number = account_number
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if self.__balance >= amount:
+            self.__balance -= amount
+        else:
+            raise('Insufficent funds')
+    
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount("1245", 8000)
+
+try:
+    print(account.__acount_number)
+except AttributeError as e:
+    print("Attribute Error", e)
